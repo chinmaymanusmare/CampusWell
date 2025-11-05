57e274daf86ba2b2918333a2beb831ac7e56b94c
Chinmay Manusmare <manusmarechinmay@gmail.com>
2025-11-06 04:02:44 +0530

    Git integrartion issue fix 18

--- pgadmin/create table.sql
+++ pgadmin/create table.sql
@@ -42,6 +42,21 @@ CREATE TABLE appointments (
 );
 
 -- =========================================
+--  DOCTOR AVAILABILITY TABLE
+-- =========================================
+CREATE TABLE doctor_availability (
+    id SERIAL PRIMARY KEY,
+    doctor_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    max_patients INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (doctor_id, date, start_time, end_time)
+);
+
+
+-- =========================================
 --  PRESCRIPTIONS TABLE
 -- =========================================
 CREATE TABLE prescriptions (
